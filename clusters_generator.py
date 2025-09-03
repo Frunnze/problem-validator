d299dd5029c1a3fa8b0f26d28f6456061cc51c40
project <vladislav.frunzze@gmail.com>
2025-09-03 20:46:51 +0300

    main flow

--- clusters_generator.py
+++ clusters_generator.py
@@ -4,13 +4,13 @@ from sklearn.metrics.pairwise import cosine_distances
 
 def get_clustered_reviews(reviews, embeddings):
     dist_matrix = cosine_distances(embeddings)
-    db = DBSCAN(eps=0.3, min_samples=2, metric="precomputed").fit(dist_matrix)
+    db = DBSCAN(eps=0.3, min_samples=10, metric="precomputed").fit(dist_matrix)
     labels = db.labels_
 
     clusters = {}
     for review, label in zip(reviews, labels):
+        if int(label) == -1:
+            continue
         clusters[int(label)] = clusters.get(int(label), []) + [review]
 
-    import json
-    print(json.dumps(clusters, indent=4))
     return clusters
---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+from dotenv import load_dotenv
+import os
+
+from scraper.store_scraper import StoreScraper
+from embeddings_builder import HuggingFaceEmbeddingsBuilder
+from clusters_generator import get_clustered_reviews
+from chroma_manager import ChromaManager
+
+
+load_dotenv()
+STARTING_APP_URL = os.getenv("STARTING_APP_URL")
+embedder = HuggingFaceEmbeddingsBuilder()
+chroma_manager = ChromaManager()
+
+if __name__ == "__main__":
+    # Scrape negative reviews
+    # scraper = StoreScraper(storage_manager=chroma_manager)
+    # scraper.scrape(
+    #     urls=[STARTING_APP_URL],
+    #     reviews_num=10000
+    # )
+
+    # Get the reviews and the embeddings
+    reviews, embs = chroma_manager.get_all_docs_embs()
+
+    # Generate clusters
+    clusters = get_clustered_reviews(reviews, embs)
+
+    # Output report
+    print(json.dumps(clusters, indent=4))
